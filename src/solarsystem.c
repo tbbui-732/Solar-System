ada1e968b2aa1baff149038730a14c190ce174d5
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-16 16:05:20 -0500

    Initialize a glfw window

--- src/solarsystem.c
+++ src/solarsystem.c
@@ -1,6 +1,44 @@
+// -- includes --
 #include <stdio.h>
+#include "../include/glad/glad.h"
+#include "../include/glfw/glfw3.h"
+
+// -- global variables --
+const int SCR_WIDTH = 1600;
+const int SCR_HEIGHT = 800;
+
 
 int main(void) {
-    printf("Hey there");
+    // -- initialize glfw --
+    if (!glfwInit()) {
+        printf("GLFW::INIT - Failed to initialize glfw");
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_COCOA_CHDIR_RESOURCES, GL_FALSE);
+#endif
+
+    // -- set up glfw window -- 
+    GLFWwindow* pWindow = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Solar System", NULL, NULL);
+    if (!pWindow) {
+        printf("GLFW::INIT - Failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(pWindow);
+
+    // -- render loop --
+    printf("Testing");
+    //while (!glfwWindowShouldClose(pWindow)) {
+    //    glClear(GL_COLOR_BUFFER_BIT);
+    //    glfwSwapBuffers(pWindow);
+    //    glfwPollEvents();
+    //}
+
+    glfwTerminate();
     return 0;
 }
