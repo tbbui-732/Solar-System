4b5bec6174137567df7ad7c6475085d2dd1f5628
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-17 14:18:19 -0500

    Process shader source file

---
+++ include/solar_system/shader.h
@@ -0,0 +1,11 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../glad/glad.h"
+#include "../glfw/glfw3.h"
+
+void shaderProcessFile(char* fileName, GLenum type);
+
+#endif
