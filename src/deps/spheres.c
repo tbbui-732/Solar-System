89e2058be6baffa359ca106e01dc5f1993940de5
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-18 18:30:47 -0500

    Calculate the vertices for a sphere

--- src/deps/spheres.c
+++ src/deps/spheres.c
@@ -1,12 +1,22 @@
 #include "../../include/solar_system/spheres.h"
 
+#include <math.h>
+#include <stdlib.h>
 
-void sphereBuildVertices(float radius, int sectorCount, int stackCount) {
+void sphereBuildVertices(float* vertices, float* normals, float* texCoords, float radius, int sectorCount, int stackCount) {
+    // allocate space for each array
+    vertices    = malloc(sizeof(float) * 3 * sectorCount * stackCount); // 3 to account for x, y, and z
+    normals     = malloc(sizeof(float) * 3 * sectorCount * stackCount); // same as vertices
+    texCoords   = malloc(sizeof(float) * 2 * sectorCount * stackCount); // 2 to account for s and t
+
     const float PI = acos(-1.0f);
-    
-    float tmpVertices[3 * sectorCount * stackCount];
-    int vi = 0;
+    int vertIdx = 0;
+    int normIdx = 0;
+    int texIdx  = 0;
 
+    float lengthInv = 1.0f / radius;
+
+    // sector and stack steps and angles
     float sectorStep = 2 * PI / sectorCount;
     float stackStep = PI / stackCount;
     float sectorAngle, stackAngle;
@@ -14,19 +24,35 @@ void sphereBuildVertices(float radius, int sectorCount, int stackCount) {
     // compute vertices
     for (int i = 0; i < stackCount; ++i) {
         stackAngle = PI / 2 - i * stackStep;
-        float rCos = radius * cosf(stackAngle); // r * cos(u)
-        float rSin = radius * sinf(stackAngle); // r * sin(u)
+        float rCosStackAngle = radius * cosf(stackAngle);
+        float rSinStackAngle = radius * sinf(stackAngle);
 
         for (int j = 0; j < sectorCount; ++j) {
             sectorAngle = j * sectorStep;
 
-            float x = rCos * cosf(sectorAngle);
-            float y = rCos * sinf(sectorAngle);
-            float z = rSin;
+            // calculate vertices
+            float x = rCosStackAngle * cosf(sectorAngle);
+            float y = rCosStackAngle * sinf(sectorAngle);
+            float z = rSinStackAngle;
+            vertices[vertIdx++] = x;
+            vertices[vertIdx++] = y;
+            vertices[vertIdx++] = z;
 
-            tmpVertices[vi++] = x;
-            tmpVertices[vi++] = y;
-            tmpVertices[vi++] = z;
+            // calculate normals
+            float nx = x * lengthInv;
+            float ny = y * lengthInv;
+            float nz = z * lengthInv;
+            normals[normIdx++] = nx;
+            normals[normIdx++] = ny;
+            normals[normIdx++] = nz;
+
+            // calculate texture coordinates
+            float s = (float) j / sectorCount;
+            float t = (float) i / stackCount;
+            texCoords[texIdx++] = s;
+            texCoords[texIdx++] = t;
         }
     }
+
+    return vertices
 }
